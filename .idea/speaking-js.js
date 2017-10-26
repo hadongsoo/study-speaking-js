b9d2be78067c129f1626764a4c11a427441ab06d
jeffrey.ha <jeffrey.ha@kakaocorp.com>
2017-10-26 13:24:45 +0900

    1day

---
+++ .idea/speaking-js.js
@@ -0,0 +1,19 @@
+/*
+* js has statement expression
+* */
+
+// this is statement 문
+var x;
+if (y >= 0){
+	x = y;
+} else {
+	x = -y;
+}
+
+//this is expression
+var x = y >= 0 ? y : -y ;
+
+
+//it can be arguemnts
+Function(y>= 0 ? y: -y)
+
