88d572873299adff2782906701749a727f3663b4
jeffrey.ha <jeffrey.ha@kakaocorp.com>
2017-10-27 10:30:50 +0900

    3day 01

--- speaking-js.js
+++ speaking-js.js
@@ -102,3 +102,78 @@ when you call 'Method' also use dot notation
 /*
 * primitive 원시값 vs objects 객체
 * */
+
+primitive 원시
+		boolean, numbers, paragragh, null, undifined
+
+objects
+		others
+
+object being themself. not same
+
+> var obj1 = {};
+> var obj2 = {};
+> obj1 === obj2
+		false
+> obj1 === obj1
+true
+
+but, in primitive, value is same, it's same
+
+	var prim1 = 123;
+	var prim2 = 123;
+	prim1 === prim2
+
+		true
+
+primitive =
+	true, false
+		1736, 1.351
+		'abc', "abc"
+		undefined, null
+
+		comparable,
+			immutable,
+
+
+/*
+* typeof and instanceof
+* */
+
+typeof for primitive,
+instanceof for object
+
+> typeof true
+							 'boolean'
+> typeof 'abc'
+								'string'
+		typeof {}
+		'object'
+		typeof []
+		'object'
+
+
+		but, typeof null > 'object' is.. exception. it's bug
+
+> var b = new Bar();
+> b instanceof Bar
+		true
+
+> {} instanceof Object
+		true
+
+> [] instanceof Array
+		true
+
+> [] instanceof Object
+		true
+
+> undefined instanceof Object
+		false
+> null instanceof Object
+		false
+
+/*
+* typeof 는 주로  원시값 분류, 맞나 확인할때 쓰는 듯 하고
+* instancefo 는 객체에서 들어있나? 생성되었나? 를 판단하는 연산자 인듯
+* */
